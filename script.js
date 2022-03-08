107eed977a1affe3562a70c3204f4fcf16da7ab5
Leandro Kespers <leandrokespers@gmail.com>
2022-03-08 16:09:54 -0300

    descricao da construcao do codigo detalhado

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+//passo 6 - criacao de uma function para isolar o codigo
 function initAccordion() {
-  const accordionLlist = document.querySelectorAll(".js-accordion dt");
+  // passo 1 - variavel com uma nodelist
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  // passo 5 - criacao da variavel activeClass evitando repeticoes de strings
   const activeClass = "ativo";
-  accordionLlist[0].classList.add(activeClass);
-  accordionLlist[0].nextElementSibling.classList.add(activeClass);
-  if (accordionLlist.length) {
+  // passo 4 - activeClass add ao primeiro dt e dd
+  accordionList[0].classList.add(activeClass);
+  accordionList[0].nextElementSibling.classList.add(activeClass);
+
+  // passo 8 - verificacao da variavel accordionList (se true executar codigo)
+  if (accordionList.length) {
+    // passo 3 - function que add ativo em dt e dd e retira ao clique (toggle)
     function activeAccordion() {
       this.classList.toggle(activeClass);
       this.nextElementSibling.classList.toggle(activeClass);
     }
 
-    accordionLlist.forEach((item) => {
+    // passo2 - looping add evento ao click com function de callback
+    accordionList.forEach((item) => {
       item.addEventListener("click", activeAccordion);
     });
   }
 }
 
+//passo 7 - ativacao da function
 initAccordion();
